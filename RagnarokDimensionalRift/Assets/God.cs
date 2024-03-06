6dc60ccfd5690b619cafa13f92103f805d07db74
RyukiAs <shionscars@gmail.com>
2024-03-05 20:05:25 -0600

    update

--- RagnarokDimensionalRift/Assets/God.cs
+++ RagnarokDimensionalRift/Assets/God.cs
@@ -6,12 +6,14 @@ public class God
     public string godName;
     public int grade;
     public Sprite icon;
+    public int level;
 
     public God(string godName, int grade, Sprite icon)
     {
         this.godName = godName;
         this.grade = grade;
         this.icon = icon;
+        this.level = 1;
     }
 
     // Additional properties and methods can be added as needed

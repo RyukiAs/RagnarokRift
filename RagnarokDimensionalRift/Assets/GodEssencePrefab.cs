a9176383241af41913d7d02fd7b28b4fb407fadc
RyukiAs <shionscars@gmail.com>
2024-03-19 02:26:07 -0500

    somewhat working button color change

--- RagnarokDimensionalRift/Assets/GodEssencePrefab.cs
+++ RagnarokDimensionalRift/Assets/GodEssencePrefab.cs
@@ -38,10 +38,32 @@ public class GodEssencePrefab : MonoBehaviour
                 gameController.GetAllEssenceGods().Add(this.godPass);
 
                 Transform childTransform = transform.Find("SelectEssence");
-                //Debug.Log("gameobject: " + gameObject.name);
                 Image image = childTransform.GetComponent<Image>();
+                Button button = childTransform.GetComponent<Button>();
+                //Image buttonImage = button.GetComponent<Image>();
+                //Image buttonImage = button.image;
+
                 image.color = Color.gray;
-            }else
+
+
+                ColorBlock colors = button.colors;
+
+                // Modify the alpha value of the normal color
+                Color normalColor = colors.normalColor;
+                normalColor.a = 1.0f; // Change this value as needed
+                colors.normalColor = normalColor;
+
+                // Apply the modified colors to the button
+                button.colors = colors;
+                ///float aplha = 0.5f;
+                //Color buttonColor = buttonImage.color;
+                //currentColor.a = aplha;
+                //float aplha = 1.0f;
+                //Color currentColor = image.color
+
+                Debug.Log("Changed Color to gray");
+            }
+            else
             {
                 print("This god already exists");
             }
